6e0006d85cd65c378dcaa5ea2611364f02697fee
harshkhadtar <h8234923@gmail.com>
2026-04-16 14:51:46 +0530

    Update complaintRoutes.js

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -4,14 +4,10 @@ const complaintController = require('../controllers/complaintController');
 const { verifyToken, isAdmin } = require('../middlewares/auth');
 const upload = require('../middlewares/upload');
 
-// Citizen routes
 router.post('/', verifyToken, upload.single('image'), complaintController.createComplaint);
 router.get('/my', verifyToken, complaintController.getMyComplaints);
-
-// Public route for data dashboard
 router.get('/all', complaintController.getAllComplaints);
 
-// Admin routes
 router.patch('/:id/status', verifyToken, isAdmin, complaintController.updateComplaintStatus);
 router.delete('/:id', verifyToken, isAdmin, complaintController.deleteComplaint);
 
