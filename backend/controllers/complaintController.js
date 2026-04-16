061287821fffbe04e4aff2a24b0e56a7b75fd177
harshkhadtar <h8234923@gmail.com>
2026-04-16 16:53:43 +0530

    Update complaintController.js

--- backend/controllers/complaintController.js
+++ backend/controllers/complaintController.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 // CREATE
 exports.createComplaint = (req, res) => {
     const { title, category, location, description } = req.body;
-    const userId = req.userId;
+    const userId = req.user.id;
 
     let imageUrl = null;
     if (req.file) {
